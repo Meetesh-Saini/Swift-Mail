87ee4dd05b2e0807488583485dc096777d944c51
Meetesh Saini <code.meeteshsaini@gmail.com>
2024-04-16 00:14:36 +0530

    feat(server): add mail functionality
    - Add send/receive endpoints
    - Refactor login and register endpoints
    - Removed unnecessay files

--- server/swiftmail/models/groups.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-import typing as t
-
-
-class Groups(BaseModel):
-    email: EmailStr
-    group_name: str
-    mail_group: t.List[str]
-    labels: t.List[str]
--- server/swiftmail/models/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-
-class User(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-    mobile: str
